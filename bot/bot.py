2c1b2b32d92c06ff80be5092cb38cb4a583477e9
Ivan Saydyashev <ivasay@vk.com>
2025-06-07 17:57:24 +0300

    feature: create lot without memory

--- bot/bot.py
+++ bot/bot.py
@@ -1,27 +1,63 @@
+from datetime import datetime
 from enum import Enum
-
+import re
 from telegram import Update, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (Application, ContextTypes, CommandHandler,
-                          ChatMemberHandler, ConversationHandler, CallbackQueryHandler, )
+                          ChatMemberHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, filters)
 from telegram.constants import ChatMemberStatus, ChatType
-
 
 class Bot:
     class NewLotteryState(Enum):
         READY = 0
         TEXT = 1
+        NUM_WINNERS = 2
+        MODE = 3
+        DATE = 4
+        COUNT = 5
+        PUBLISHER = 6
 
-    new_lottery = "Создать розыгрыш"
+    # new_lottery = "Создать розыгрыш"
 
     def __init__(self, app: Application) -> None:
+        self.mode_keyboard = InlineKeyboardMarkup([
+            [InlineKeyboardButton("Закончить по дате", callback_data="mode_date")],
+            [InlineKeyboardButton("Закончить по числу участников", callback_data="mode_count")],
+            [InlineKeyboardButton("Назад", callback_data="back_data")]
+        ])
+        self.back_keyboard = InlineKeyboardMarkup([
+                [InlineKeyboardButton("Назад", callback_data="back_data")]
+            ])
+
         # TODO: временное хранилище чисто для тестов надо заменить
         app.add_handler(CommandHandler("help", self.help_command))
         app.add_handler(ChatMemberHandler(self.invitation))
         app.add_handler(ConversationHandler(
             entry_points=[CommandHandler("start", self.start)],
             states={
-                Bot.NewLotteryState.READY.value: [CallbackQueryHandler(self.button_handler)],
-                Bot.NewLotteryState.TEXT.value: [],
+                Bot.NewLotteryState.READY.value: [
+                    CallbackQueryHandler(self.button_handler)
+                ],
+                Bot.NewLotteryState.TEXT.value: [
+                    MessageHandler(filters.TEXT & ~filters.COMMAND, self.lottery_text),
+                ],
+                Bot.NewLotteryState.NUM_WINNERS.value: [
+                    CallbackQueryHandler(self.lottery_num_winners),
+                    MessageHandler(filters.TEXT & ~filters.COMMAND, self.lottery_num_winners),
+                ],
+                Bot.NewLotteryState.MODE.value: [
+                    CallbackQueryHandler(self.lottery_mode)
+                ],
+                Bot.NewLotteryState.DATE.value: [
+                    CallbackQueryHandler(self.lottery_date),
+                    MessageHandler(filters.TEXT & ~filters.COMMAND, self.lottery_date),
+                ],
+                Bot.NewLotteryState.COUNT.value: [
+                    CallbackQueryHandler(self.lottery_count),
+                    MessageHandler(filters.TEXT & ~filters.COMMAND, self.lottery_count),
+                ],
+                Bot.NewLotteryState.PUBLISHER.value: [
+                    CallbackQueryHandler(self.lottery_publisher),
+                ]
             },
             fallbacks=[],
         ))
@@ -30,12 +66,12 @@ class Bot:
 
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         """Send a message when the command /start is issued."""
-        keyboard = [[Bot.new_lottery]]
-        keyboard = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+        # keyboard = [[Bot.new_lottery]]
+        # keyboard = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
         await update.message.reply_text(
             "Привет! Я помогу тебе создать розыгрыш в Telegram канале или чате! "
             "Нажимай на кнопки и следуй инструкциям:",
-            reply_markup=keyboard
+            reply_markup=None
         )
         await self.create_channel_list_message(update, context)
         return Bot.NewLotteryState.READY.value
@@ -43,9 +79,6 @@ class Bot:
     async def help_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         """Send a message when the command /help is issued."""
         await update.message.reply_text("Help!")
-
-    async def lottery_text(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        print("ready text")
 
     async def invitation(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         match ChatMemberStatus(update.my_chat_member.new_chat_member.status):
@@ -152,12 +185,133 @@ class Bot:
     async def button_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         query = update.callback_query
         user = update.effective_user
-        if query.data == "ready":
-            channels = context.bot_data[user.id]["channels"]
-            if not all(map(lambda c: c["status"] == ChatMemberStatus.ADMINISTRATOR.value, channels)):
-                await query.answer("Бот должен быть администратором во всех каналах, где он есть!",
-                                   show_alert=True)
-                return None
-            else:
+
+        channels = context.bot_data[user.id]["channels"]
+        if not channels:
+            await query.answer("Добавьте бота как администратора хотя бы в один канал!", show_alert=True)
+            return Bot.NewLotteryState.READY.value
+        if not all(c["status"] == ChatMemberStatus.ADMINISTRATOR for c in channels):
+            await query.answer("Бот должен быть администратором во всех каналах, где он есть!", show_alert=True)
+            return Bot.NewLotteryState.READY.value
+
+        await query.edit_message_text("Отлично! Теперь отправьте описание розыгрыша.")
+
+        return Bot.NewLotteryState.TEXT.value
+
+    async def lottery_text(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        description = update.message.text # TODO: Сохранить описание в бд
+
+        await update.message.reply_text("Отлично! Теперь отправьте количество победителей числом.",
+                                        reply_markup=self.back_keyboard)
+        return Bot.NewLotteryState.NUM_WINNERS.value
+
+    async def lottery_num_winners(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        if update.callback_query:
+            query = update.callback_query
+            if query.data == "back_data":
+                await query.edit_message_text("Отлично! Теперь отправьте описание розыгрыша.")
                 return Bot.NewLotteryState.TEXT.value
-        return None
+            return Bot.NewLotteryState.NUM_WINNERS.value
+
+        num_winners = update.message.text
+        if num_winners.isdigit(): # TODO: Сохранить число в бд
+            await update.message.reply_text("Как будет определяться конец розыгрыша?", reply_markup=self.mode_keyboard)
+            return Bot.NewLotteryState.MODE.value
+
+        await update.message.reply_text("Неверный формат. Введите число победителей (целое число).",
+                                        reply_markup=self.back_keyboard)
+        return Bot.NewLotteryState.NUM_WINNERS.value
+
+    async def lottery_mode(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        query = update.callback_query
+
+        if query.data == "mode_date":
+            await query.edit_message_text("Отправьте дату окончания розыгрыша в формате дд.мм.гггг чч:мм, "
+                                          "например, 01.01.2024 12:00.", reply_markup=self.back_keyboard)
+            return Bot.NewLotteryState.DATE.value
+        elif query.data == "mode_count":
+            await query.edit_message_text("Отправьте максимальное количество участников (числом).", reply_markup=self.back_keyboard)
+            return Bot.NewLotteryState.COUNT.value
+
+        await query.edit_message_text("Отлично! Теперь отправьте количество победителей числом.",
+                                      reply_markup=self.back_keyboard)
+        return Bot.NewLotteryState.NUM_WINNERS.value
+
+    async def lottery_count(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        if update.callback_query:
+            query = update.callback_query
+            if query.data == "back_data":
+                await query.edit_message_text("Как будет определяться конец розыгрыша?",
+                                              reply_markup=self.mode_keyboard)
+                return Bot.NewLotteryState.MODE.value
+            return Bot.NewLotteryState.COUNT.value
+
+        count_str = update.message.text
+        publisher_keyboard = self.get_publisher_channels_keyboard(update, context)
+        if count_str.isdigit(): # TODO: Сохранить число в бд
+            await update.message.reply_text("Отлично! Укажите канал, который опубликует результаты розыгрыша!",
+                                            reply_markup=publisher_keyboard)
+            return Bot.NewLotteryState.PUBLISHER.value
+
+        await update.message.reply_text("Неверный формат. Введите максимальное число участников (целое число).",
+                                        reply_markup=self.back_keyboard)
+        return Bot.NewLotteryState.COUNT.value
+
+    async def lottery_date(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        if update.callback_query:
+            query = update.callback_query
+            if query.data == "back_data":
+                await query.edit_message_text("Как будет определяться конец розыгрыша?",
+                                              reply_markup=self.mode_keyboard)
+                return Bot.NewLotteryState.MODE.value
+            return Bot.NewLotteryState.DATE.value
+
+        date_str = update.message.text
+        date = self.parse_date(date_str) # TODO: Сохранить дату в бд
+        publisher_keyboard = self.get_publisher_channels_keyboard(update, context)
+        if date:
+            await update.message.reply_text("Отлично! Укажите канал, который опубликует результаты розыгрыша!",
+                                            reply_markup=publisher_keyboard)
+            return Bot.NewLotteryState.PUBLISHER.value
+
+        await update.message.reply_text(
+            "Не удалось распознать дату. Пожалуйста, отправьте в формате дд.мм.гггг чч:мм, "
+            "например, 01.01.2024 12:00.", reply_markup=self.back_keyboard)
+        return Bot.NewLotteryState.DATE.value
+
+    def parse_date(self, date_str: str) -> datetime | None:
+        """
+        Преобразует строку в datetime, если она соответствует формату 'dd.mm.yyyy hh:mm',
+        иначе возвращает None.
+        """
+        try:
+            return datetime.strptime(date_str, "%d.%m.%Y %H:%M")
+        except ValueError:
+            return None
+
+    async def lottery_publisher(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        query = update.callback_query
+
+        if query.data == "back_data":
+            await query.edit_message_text("Как будет определяться конец розыгрыша?",
+                                          reply_markup=self.mode_keyboard)
+            return Bot.NewLotteryState.MODE.value
+        # TODO: Сохранить канал в бд
+        await context.bot.send_message(chat_id=query.from_user.id, text="Отлично! Розыгрыш создан!")
+        return ConversationHandler.END
+
+    def get_publisher_channels_keyboard(self, update: Update,
+                                        context: ContextTypes.DEFAULT_TYPE) -> InlineKeyboardMarkup:
+        """
+        Возвращает клавиатуру с каналами пользователя, которые могут быть выбраны в качестве публикатора.
+        """
+        channels = context.bot_data[update.effective_user.id]["channels"]
+        keyboard = []
+        for i, channel in enumerate(channels):
+            match = re.search(r'>([^<]+)<', channel["username"])
+            title = match.group(1) if match else "Канал"
+
+            button = InlineKeyboardButton(text=title, callback_data=f"{channel['chat_id']}")
+            keyboard.append([button])  # каждая кнопка в отдельной строке
+
+        return InlineKeyboardMarkup(keyboard)
